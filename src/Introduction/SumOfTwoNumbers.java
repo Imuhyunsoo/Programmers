633dbc65d258ab7ddc60e6dfe71711dea04c5084
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-20 23:30:11 +0900

    SumOfTwoNumbers method create

---
+++ src/Introduction/SumOfTwoNumbers.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+public class SumOfTwoNumbers {
+
+    public static int addTwoNumbers(int num1, int num2) {
+
+        int answer = 0;
+
+        if((num1 >= -50000 & num2 <= 50000) & (num2 >= -50000 & num2 <= 50000)) {
+            answer = num1 + num2;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        // TEST CODE
+        System.out.println(addTwoNumbers(2,3));
+        System.out.println(addTwoNumbers(100,2));
+    }
+}
