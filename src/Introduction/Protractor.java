4dbc60b45ca3768077c4fdbe777c1286d8fc1b79
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-21 22:00:41 +0900

    Protractor method create

---
+++ src/Introduction/Protractor.java
@@ -0,0 +1,30 @@
+package Introduction;
+
+public class Protractor {
+
+    public static int angleChick(int angle) {
+
+        int answer = 0;
+
+        if(angle > 0 & angle < 90) {
+            answer = 1;
+        } else if(angle == 90) {
+            answer = 2;
+        } else if(angle > 90 & angle < 180) {
+            answer = 3;
+        } else if(angle == 180) {
+            answer = 4;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        // TEST CODE
+        System.out.println(angleChick(70));
+        System.out.println(angleChick(91));
+        System.out.println(angleChick(180));
+    }
+}
