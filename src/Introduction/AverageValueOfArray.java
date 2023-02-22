71f29c1cb48515695dda2718a105d18c0dbf36a6
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-22 21:31:57 +0900

    AverageValueOfArray class create

--- src/Introduction/AverageValueOfArray.java
+++ src/Introduction/AverageValueOfArray.java
@@ -2,20 +2,23 @@ package Introduction;
 
 public class AverageValueOfArray {
 
-    public double average(int[] numbers) {
+    public static double average(int[] numbers) {
 
         double answer = 0;
+        double sum = 0;
 
+        // Restrictions
         if(numbers.length >= 1 & numbers.length <= 100) {
 
-            for(int i = 0; i < numbers.length; i++) {
+            for (int number : numbers) {
 
-                if(!(numbers[i] >= 0 & numbers[i] <= 1000)) {
-                    answer = 0;
+                // Restrictions
+                if (!(number >= 0 & number <= 1000)) {
                     break;
                 }
-
+                sum += number;
             }
+            answer = sum / numbers.length;
         }
 
         return answer;
@@ -23,5 +26,12 @@ public class AverageValueOfArray {
 
     public static void main(String[] args) {
 
+        int[] array;
+
+        // TEST CODE
+        array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(average(array));
+        array = new int[] {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+        System.out.println(average(array));
     }
 }
