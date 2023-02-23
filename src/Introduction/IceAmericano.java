4765e3b5a8fff2d5e58a8dbcdcf031d16840e129
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-23 16:04:53 +0900

    IceAmericano class create

--- src/Introduction/IceAmericano.java
+++ src/Introduction/IceAmericano.java
@@ -1,2 +1,27 @@
-package Introduction;public class IceAmericano {
+package Introduction;
+
+import java.util.Arrays;
+
+public class IceAmericano {
+
+    public static int[] maximumBuy(int money) {
+
+        int[] answer = new int[2];
+
+        // Restrictions
+        if(money > 0 & money <= 1000000) {
+
+            answer[0] = money / 5500;
+            answer[1] = money % 5500;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        // TEST CODE
+        System.out.println(Arrays.toString(maximumBuy(5500)));
+        System.out.println(Arrays.toString(maximumBuy(15000)));
+    }
 }
