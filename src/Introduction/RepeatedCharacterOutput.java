89fe40a4799b7f6e77414bc0cb5b50feb67f3ca0
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-23 22:15:23 +0900

    RepeatedCharacterOutput class create

---
+++ src/Introduction/RepeatedCharacterOutput.java
@@ -0,0 +1,24 @@
+package Introduction;
+
+public class RepeatedCharacterOutput {
+
+    public static String repeatedOutput(String my_string, int n) {
+
+        StringBuilder answer = new StringBuilder();
+
+        if(my_string.length() >= 2 & my_string.length() <= 5 & n >= 2 & n <= 10) {
+
+            for(int i = 0; i < my_string.length(); i++) {
+                answer.append(String.valueOf(my_string.charAt(i)).repeat(n));
+            }
+        }
+
+        return answer.toString();
+    }
+
+    public static void main(String[] args) {
+
+        // TEST CODE
+        System.out.println(repeatedOutput("hello",3));
+    }
+}
