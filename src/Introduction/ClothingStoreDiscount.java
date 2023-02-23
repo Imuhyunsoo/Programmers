85a620ab5748c4070589065715ca97f10dabf0ed
ThinkingMoomin <qnddj0213@gmail.com>
2023-02-23 15:57:42 +0900

    ClothingStoreDiscount class create

---
+++ src/Introduction/ClothingStoreDiscount.java
@@ -0,0 +1,31 @@
+package Introduction;
+
+public class ClothingStoreDiscount {
+
+    public static int getPay(int price) {
+
+        int answer = 0;
+
+        // Restrictions
+        if(price >= 10 & price <= 1000000) {
+            
+            if(price >= 100000 & price < 300000) {
+                answer = (int) (price - (price * 0.05));
+            } else if(price >= 300000 & price < 500000) {
+                answer = (int) (price - (price * 0.1));
+            } else if(price >= 500000) {
+                answer = (int) (price - (price * 0.2));
+            } else {
+                answer = price;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        // TEST CODE
+        System.out.println(getPay(150000));
+        System.out.println(getPay(580000));
+    }
+}
