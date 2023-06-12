e66187cc4e80c80ecadb2b039ee987b0220539f2
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-12 12:54:05 +0900

    OutputAAndB class create

---
+++ src/CodingBasicTraining/OutputAAndB.java
@@ -0,0 +1,18 @@
+package CodingBasicTraining;
+
+import java.util.Scanner;
+
+public class OutputAAndB {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // 制限事項
+        if (!(a < -100000 | b < -100000 | a > 100000 | b > 100000)) {
+            System.out.println("a = " + a + '\n' + "b = " + b);
+        }
+    }
+}
