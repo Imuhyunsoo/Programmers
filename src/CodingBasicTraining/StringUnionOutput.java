d11914d56eb9bd4e0aa1f0087d883cccee438ed3
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-13 11:28:14 +0900

    StringUnionOutput class create

---
+++ src/CodingBasicTraining/StringUnionOutput.java
@@ -0,0 +1,15 @@
+package CodingBasicTraining;
+
+import java.util.Scanner;
+
+public class StringUnionOutput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a.concat(b));
+    }
+}
