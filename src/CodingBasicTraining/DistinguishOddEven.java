80f716a1f3cc655577ef78eea196593f8c74cd23
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-13 11:34:11 +0900

    DistinguishOddEven class create

---
+++ src/CodingBasicTraining/DistinguishOddEven.java
@@ -0,0 +1,17 @@
+package CodingBasicTraining;
+
+import java.util.Scanner;
+
+public class DistinguishOddEven {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
+}
