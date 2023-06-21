48fb7bab1d9bf09fd35b39f4b980e0e8224b7e2d
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-21 17:49:50 +0900

    SQL問題：胸部外科または一般外科医者リスト出力する、解決

--- src/SQL_HighScoreKit/Select/select.sql
+++ src/SQL_HighScoreKit/Select/select.sql
@@ -66,3 +66,18 @@ WHERE INFO.REST_ID = REVIEW.REST_ID
 GROUP BY INFO.REST_ID, INFO.REST_NAME, INFO.FOOD_TYPE, INFO.FAVORITES, INFO.ADDRESS
 HAVING AVG(REVIEW.REVIEW_SCORE) > 0
 ORDER BY SCORE DESC, FAVORITES DESC;
+
+
+
+-- 흉부외과 또는 일반외과 의사 목록 출력하기
+-- 胸部外科または一般外科医者リスト出力する
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132203?language=oracle
+
+-- 問題
+-- DOCTOR 테이블에서 진료과가 흉부외과(CS)이거나 일반외과(GS)인 의사의 이름, 의사 ID, 진료과, 고용일자를 조회하는 SQL 문을 작성해주세요.
+-- 이때 결과는 고용일자를 기준으로 내림차순 정렬하고, 고용일자가 같다면 이름을 기준으로 오름차순 정렬해주세요.
+
+SELECT DR_NAME, DR_ID, MCDP_CD, TO_CHAR(HIRE_YMD, 'YYYY-MM-DD') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD IN ('CS', 'GS')
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
