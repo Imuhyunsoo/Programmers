ec3d123b9e1c2e98df44e5e7839ca36b07a53b80
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-21 12:56:16 +0900

    SQL問題：韓湾道に位置にある生産工場の目録を出力する、解決

--- src/SQL_HighScoreKit/Select/select.sql
+++ src/SQL_HighScoreKit/Select/select.sql
@@ -2,6 +2,7 @@
 -- 三月に生まれた女性会員リスト出力
 -- https://school.programmers.co.kr/learn/courses/30/lessons/131120
 
+-- 問題
 -- MEMBER_PROFILE 테이블에서 생일이 3월인 여성 회원의 ID, 이름, 성별, 생년월일을 조회하는 SQL 문을 작성해주세요.
 -- 이때 전화번호가 NULL 인 경우는 출력대상에서 제외시켜 주시고, 결과는 회원 ID를 기준으로 오름차순 정렬해주세요.
 
@@ -12,11 +13,13 @@ WHERE EXTRACT(MONTH FROM DATE_OF_BIRTH) = 3
   AND GENDER LIKE 'W'
 ORDER BY MEMBER_ID ASC;
 
+
+
 -- 조건에 부합하는 중고거래 댓글 조회하기
 -- 条件に合致する中古取引コメント照会する
 -- https://school.programmers.co.kr/learn/courses/30/lessons/164673
 
--- 문제
+-- 問題
 -- USED_GOODS_BOARD 와 USED_GOODS_REPLY 테이블에서 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용,
 -- 댓글 작성일을 조회하는 SQL 문을 작성해주세요. 결과는 댓글 작성일을 기준으로 오름차순 정렬해주시고,
 -- 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬해주세요.
@@ -28,3 +31,18 @@ WHERE BO.BOARD_ID = RE.BOARD_ID
   AND EXTRACT(YEAR FROM BO.CREATED_DATE) = 2022
   AND EXTRACT(MONTH FROM BO.CREATED_DATE) = 10
 ORDER BY RE.CREATED_DATE ASC, BO.TITLE ASC;
+
+
+
+-- 강원도에 위치한 생산공장 목록 출력하기
+-- 韓湾道に位置にある生産工場の目録を出力する
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131112
+
+-- 問題
+-- FOOD_FACTORY 테이블에서 강원도에 위치한 식품공장의 공장 ID, 공장 이름, 주소를 조회하는 SQL 문을 작성해주세요.
+-- 이때 결과는 공장 ID를 기준으로 오름차순 정렬해주세요.
+
+SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+FROM FOOD_FACTORY
+WHERE SUBSTR(ADDRESS, 1, 3) LIKE '강원도'
+ORDER BY FACTORY_ID ASC;
