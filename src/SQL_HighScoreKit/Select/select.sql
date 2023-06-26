43262274a174f882114004c405de9ee156e2bb91
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-27 08:59:52 +0900

    SQL問題：痛い動物探し、解決

--- src/SQL_HighScoreKit/Select/select.sql
+++ src/SQL_HighScoreKit/Select/select.sql
@@ -225,3 +225,17 @@ FROM (SELECT TO_CHAR(SALES_DATE, 'YYYY-MM-DD') AS SALES_DATE, PRODUCT_ID, USER_I
       FROM OFFLINE_SALE)
 WHERE SALES_DATE LIKE '2022-03%'
 ORDER BY SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC;
+
+
+
+-- 아픈 동물 찾기
+-- 痛い動物探し
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59036?language=oracle
+
+-- 問題
+-- 동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION LIKE 'Sick'
+ORDER BY ANIMAL_ID ASC;
