4c7c388ffded3bce1e89a31f3af55ed9a6f57d3f
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-26 11:15:23 +0900

    SQL問題：オフライン/オンライン販売データ統合する、解決

--- src/SQL_HighScoreKit/Select/select.sql
+++ src/SQL_HighScoreKit/Select/select.sql
@@ -203,3 +203,25 @@ ORDER BY USER_ID ASC, PRODUCT_ID DESC;
 SELECT NAME, DATETIME
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID DESC;
+
+
+
+-- 오프라인/온라인 판매 데이터 통합하기
+-- オフライン/オンライン販売データ統合する
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131537?language=oracle
+
+-- 問題
+-- ONLINE_SALE 테이블과 OFFLINE_SALE 테이블에서 2022년 3월의 오프라인/온라인 상품 판매 데이터의
+-- 판매 날짜, 상품 ID, 유저 ID, 판매량을 출력하는 SQL 문을 작성해주세요.
+-- OFFLINE_SALE 테이블의 판매 데이터의 USER_ID 값은 NULL 로 표시해주세요.
+-- 결과는 판매일을 기준으로 오름차순 정렬해주시고 판매일이 같다면 상품 ID를 기준으로 오름차순,
+-- 상품 ID 까지 같다면 유저 ID를 기준으로 오름차순 정렬해주세요.
+
+SELECT SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+FROM (SELECT TO_CHAR(SALES_DATE, 'YYYY-MM-DD') AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+      FROM ONLINE_SALE
+      UNION ALL
+      SELECT TO_CHAR(SALES_DATE, 'YYYY-MM-DD') AS SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT
+      FROM OFFLINE_SALE)
+WHERE SALES_DATE LIKE '2022-03%'
+ORDER BY SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC;
