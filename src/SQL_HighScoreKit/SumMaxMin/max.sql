254f64f447eb87e333a8128faf9377497737a860
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-28 09:37:08 +0900

    SQL問題：一番高い商品を求める、解決

--- src/SQL_HighScoreKit/SumMaxMin/max.sql
+++ src/SQL_HighScoreKit/SumMaxMin/max.sql
@@ -8,3 +8,15 @@
 SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
 FROM FOOD_PRODUCT
 WHERE PRICE LIKE (SELECT MAX(PRICE) FROM FOOD_PRODUCT);
+
+
+
+-- 가장 비싼 상품 구하기
+-- 一番高い商品を求める
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131697?language=oracle
+
+-- 問題
+-- PRODUCT 테이블에서 판매 중인 상품 중 가장 높은 판매가를 출력하는 SQL문을 작성해주세요. 이때 컬럼명은 MAX_PRICE로 지정해주세요.
+
+SELECT MAX(PRICE) AS MAX_PRICE
+FROM PRODUCT;
