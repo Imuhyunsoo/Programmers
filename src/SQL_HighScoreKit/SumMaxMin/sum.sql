98cb543a7e47f0f4115466ae53a731e37bec9fdd
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-28 10:03:20 +0900

    SQL問題：動物の数を求める、解決

---
+++ src/SQL_HighScoreKit/SumMaxMin/sum.sql
@@ -0,0 +1,9 @@
+-- 동물 수 구하기
+-- 動物の数を求める
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59406?language=oracle
+
+-- 問題
+-- 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
+
+SELECT COUNT(*) AS COUNT
+FROM ANIMAL_INS;
