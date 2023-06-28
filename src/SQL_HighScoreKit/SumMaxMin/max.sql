b620874556e15979c4634b8ef9be4f0b1d23a1b0
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-28 09:43:16 +0900

    SQL問題：最大値求める、解決

--- src/SQL_HighScoreKit/SumMaxMin/max.sql
+++ src/SQL_HighScoreKit/SumMaxMin/max.sql
@@ -20,3 +20,15 @@ WHERE PRICE LIKE (SELECT MAX(PRICE) FROM FOOD_PRODUCT);
 
 SELECT MAX(PRICE) AS MAX_PRICE
 FROM PRODUCT;
+
+
+
+-- 최댓값 구하기
+-- 最大値求める
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59415
+
+-- 問題
+-- 가장 최근에 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+
+SELECT MAX(DATETIME) AS DATETIME
+FROM ANIMAL_INS;
