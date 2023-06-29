b66c139ce63433a08d523714a286f3c7592b6c38
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-29 09:33:07 +0900

    SQL問題：名前がない動物のID、解決

--- src/SQL_HighScoreKit/IsNull/isnull.sql
+++ src/SQL_HighScoreKit/IsNull/isnull.sql
@@ -10,3 +10,17 @@ SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS, NVL(FREEZER_YN, 'N') AS FREEZER_YN
 FROM FOOD_WAREHOUSE
 WHERE ADDRESS LIKE '%경기%'
 ORDER BY WAREHOUSE_ID ASC;
+
+
+
+-- 이름이 없는 동물의 아이디
+-- 名前がない動物のID
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59039
+
+-- 問題
+-- 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요. 단, ID는 오름차순 정렬되어야 합니다.
+
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID ASC;
