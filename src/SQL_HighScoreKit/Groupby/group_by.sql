861513ba9a47566e62eae6ec1756e7e363dd1eed
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-30 11:00:38 +0900

    SQL問題：自動車の種類別、特定オプションが含まれた自動車の数求める、解決

--- src/SQL_HighScoreKit/Groupby/group_by.sql
+++ src/SQL_HighScoreKit/Groupby/group_by.sql
@@ -50,3 +50,23 @@ FROM (SELECT CATEGORY, MAX(PRICE) AS MAX_PRICE
 WHERE MAX.CATEGORY = FOOD.CATEGORY
   AND MAX.MAX_PRICE = FOOD.PRICE
 ORDER BY MAX.MAX_PRICE DESC;
+
+
+
+-- 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
+-- 自動車の種類別、特定オプションが含まれた自動車の数求める
+-- https://school.programmers.co.kr/learn/courses/30/lessons/151137?language=oracle
+
+-- 問題
+-- CAR_RENTAL_COMPANY_CAR 테이블에서 '통풍시트', '열선시트', '가죽시트' 중 하나 이상의 옵션이 포함된 자동차가
+-- 자동차 종류 별로 몇 대인지 출력하는 SQL문을 작성해주세요.
+-- 이때 자동차 수에 대한 컬럼명은 CARS로 지정하고, 결과는 자동차 종류를 기준으로 오름차순 정렬해주세요.
+
+SELECT CAR_TYPE, COUNT(*) CARS
+FROM CAR_RENTAL_COMPANY_CAR
+-- WHERE OPTIONS REGEXP ('통풍시트|열선시트|가죽시트') BEST ANSWER
+WHERE OPTIONS LIKE '%통풍시트%'
+   OR OPTIONS LIKE '%열선시트%'
+   OR OPTIONS LIKE '%가죽시트%'
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC;
