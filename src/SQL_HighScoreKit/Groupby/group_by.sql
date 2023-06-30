2e70955e903cb1fdaff46f6dfcb43eb7ab4f4d23
イム・ヒョンス <qnddj0213@gmail.com>
2023-06-30 09:51:21 +0900

    SQL問題：成分で区別したアイスの総注文量、解決

--- src/SQL_HighScoreKit/Groupby/group_by.sql
+++ src/SQL_HighScoreKit/Groupby/group_by.sql
@@ -15,3 +15,19 @@ WHERE B.BOOK_ID = S.BOOK_ID
   AND EXTRACT(MONTH FROM S.SALES_DATE) = 01
 GROUP BY B.AUTHOR_ID, A.AUTHOR_NAME, B.CATEGORY
 ORDER BY B.AUTHOR_ID ASC, B.CATEGORY DESC;
+
+
+
+-- 성분으로 구분한 아이스크림 총 주문량
+-- 成分で区別したアイスの総注文量
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133026?language=oracle
+
+-- 問題
+-- 상반기 동안 각 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량을 총주문량이 작은 순서대로 조회하는 SQL 문을 작성해주세요.
+-- 이때 총주문량을 나타내는 컬럼명은 TOTAL_ORDER로 지정해주세요.
+
+SELECT INFO.INGREDIENT_TYPE, SUM(HALF.TOTAL_ORDER) AS TOTAL_ORDER
+FROM FIRST_HALF HALF, ICECREAM_INFO INFO
+WHERE HALF.FLAVOR = INFO.FLAVOR
+GROUP BY INFO.INGREDIENT_TYPE
+ORDER BY TOTAL_ORDER ASC;
